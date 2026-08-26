c581e62e3793183a437a743228ba3c5d19dfa09a
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-26 18:54:41 +0300

    Use semantic icons for scheduled endpoints

--- include/servicelib/runtime/status/status.hpp
+++ include/servicelib/runtime/status/status.hpp
@@ -142,6 +142,8 @@ inline constexpr std::string_view kApiIcon =
 
 inline constexpr std::string_view kCallMadeIcon =
     "M9,5V7H15.59L4,18.59L5.41,20L17,8.41V15H19V5";
+inline constexpr std::string_view kCalendarClockIcon =
+    "M15,13H16.5V15.82L18.94,17.23L18.19,18.53L15,16.69V13M19,8H5V19H9.67C9.24,18.09 9,17.07 9,16A7,7 0 0,1 16,9C17.07,9 18.09,9.24 19,9.67V8M5,21C3.89,21 3,20.1 3,19V5C3,3.89 3.89,3 5,3H6V1H8V3H16V1H18V3H19A2,2 0 0,1 21,5V11.1C22.24,12.36 23,14.09 23,16A7,7 0 0,1 16,23C14.09,23 12.36,22.24 11.1,21H5M16,11.15A4.85,4.85 0 0,0 11.15,16C11.15,18.68 13.32,20.85 16,20.85A4.85,4.85 0 0,0 20.85,16C20.85,13.32 18.68,11.15 16,11.15Z";
 
 inline std::string_view IconPath(api::TransformationType type) noexcept {
   constexpr std::string_view kDatabaseArrowRight =
@@ -289,6 +291,15 @@ inline bool StreamIconIsApi(const config::RuntimeConfig& runtime,
 inline std::string_view StreamIconPath(
     const config::RuntimeConfig& runtime,
     const config::StreamConfigRef& stream) noexcept {
+  int endpoint_id = 0;
+  if (const auto* input = stream.As<config::InputStreamConfig>()) endpoint_id = input->idEndpoint;
+  else if (const auto* sink = stream.As<config::SinkStreamConfig>()) endpoint_id = sink->idEndpoint;
+  if (const auto endpoint = runtime.GetEndpointConfigByID(endpoint_id)) {
+    if (const auto connector = runtime.GetDataConnectorByID(endpoint->GetIdDataConnector());
+        connector && connector->GetType() == api::DataConnectorType::kCron) {
+      return kCalendarClockIcon;
+    }
+  }
   if (StreamIconIsApi(runtime, stream)) {
     return stream.GetType() == api::TransformationType::kSink
                ? kCallMadeIcon
