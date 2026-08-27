6a1fd69409d7ef2221c74ac602d8d3292f548105
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-27 08:37:14 +0300

    Route userver source through Git mirror

--- scripts/dependency-proxy-env.sh
+++ scripts/dependency-proxy-env.sh
@@ -4,6 +4,8 @@ if [[ -n "${SERVICEGEN_DEPENDENCY_PROXY_DIR:-}" ]]; then
   servicegen_proxy_docker_host="${SERVICEGEN_DEPENDENCY_PROXY_DOCKER_HOST:-host.docker.internal}"
   servicegen_proxy_port="${SERVICEGEN_DEPENDENCY_PROXY_PORT:-${SERVICEGEN_NEXUS_PORT:-18081}}"
   servicegen_proxy_base="http://${servicegen_proxy_docker_host}:${servicegen_proxy_port}/repository"
+  servicegen_git_mirror_port="${SERVICEGEN_GIT_MIRROR_PORT:-18084}"
+  servicegen_git_mirror="http://${servicegen_proxy_docker_host}:${servicegen_git_mirror_port}/cgi-bin/git"
 
   export SERVICEGEN_CONAN_HOME="${SERVICEGEN_DEPENDENCY_PROXY_DIR}/conan2"
   export SERVICEGEN_GITHUB_RAW_URL="${servicegen_proxy_base}/github-raw"
@@ -13,4 +15,13 @@ if [[ -n "${SERVICEGEN_DEPENDENCY_PROXY_DIR:-}" ]]; then
   export SERVICEGEN_APT_UBUNTU_ARCHIVE_URL="${servicegen_proxy_base}/apt-ubuntu-archive"
   export SERVICEGEN_APT_UBUNTU_SECURITY_URL="${servicegen_proxy_base}/apt-ubuntu-security"
   export SERVICEGEN_APT_UBUNTU_PORTS_URL="${servicegen_proxy_base}/apt-ubuntu-ports"
+  if [[ -z "${USERVER_SOURCE_CONTEXT:-}" ]]; then
+    servicegen_proxy_script_dir="$(CDPATH= cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+    servicegen_userver_revision="$(sed -nE \
+      's|.*userver\.git#([0-9a-f]+).*|\1|p' \
+      "${servicegen_proxy_script_dir}/../docker-compose.cmake.yml" | head -n 1)"
+    if [[ -n "${servicegen_userver_revision}" ]]; then
+      export USERVER_SOURCE_CONTEXT="${servicegen_git_mirror}/github.com/userver-framework/userver.git#${servicegen_userver_revision}"
+    fi
+  fi
 fi
